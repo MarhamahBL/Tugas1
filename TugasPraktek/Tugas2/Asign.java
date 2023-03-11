f8ee8892e61271f36232cff2373e3d6680d057a6
MarhamahBL <127584572+MarhamahBL@users.noreply.github.com>
2023-03-12 01:31:14 +0800

    Add files via upload

---
+++ TugasPraktek/Tugas2/Asign.java
@@ -0,0 +1,23 @@
+/*NIM   : 13020210245
+Nama    : Marhamah BL
+Hari/Tgl: Sabtu, 11 Maret 2023
+Waktu   : 19: 50 pm/
+package tugas2;
+
+public class Asign {
+
+    /**
+     * @param args
+     */
+
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* kamus */
+        int i;
+        /* Program */
+        System.out.print("hello\n"); i=5;
+        System.out.println("Ini nilai i : " +i);
+    }
+    
+}
